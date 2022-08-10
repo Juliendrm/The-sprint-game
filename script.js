0fe813fe43f09a2bf747509bfaffd0cef6c68dd3
Julien <benedetjulien@gmail.com>
2022-08-10 10:36:18 +0200

    Now I can generate a random key touch, all lowercases uppercases and numbers from 0 to 9 include

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const startBtn = document.querySelector(`.btn`);
 canvas.width = document.body.clientWidth;
 let animationId;
 
+// Initialization of the runner
 const runner1 = new Image();
 runner1.src = "/images/Sprite.png";
 const sprite1 = {
   width: 128,
   height: 165,
-  frameRate: 30,
-  speed: 2,
+  frameRate: 50,
+  speed: 1,
   x: 0,
   y: 165,
   lastTimestamp: null,
@@ -51,17 +52,19 @@ function updateCanvas(timestamp) {
   sprite1.draw(timestamp);
 
   document.querySelector("#distance span").textContent = (
-    sprite1.x / 4
+    sprite1.x / 5
   ).toFixed(2);
   sprite1.x; // Get the distance with the runner moving
   animationId = requestAnimationFrame(updateCanvas);
   //   console.log(animationId);
 }
 
+//Display character running when start button is clicked
 startBtn.addEventListener("click", () => {
   updateCanvas();
 });
 
+//Countdown of 30seconds when player clicked start button
 let count = document.querySelector("#countdown");
 let i = 30;
 count.textContent = 30;
@@ -73,3 +76,73 @@ startBtn.addEventListener("click", () => {
     count.textContent = `${i}`;
   }, 1000);
 });
+
+// Get a random keyboard touch (uppercasesm lowercases and numbers rom 0 to 9);
+let array = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+];
+
+const key = document.querySelector(".randomKey span");
+let randomKey = Math.floor(Math.random() * array.length);
+key.textContent = array[randomKey];
